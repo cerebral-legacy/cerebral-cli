61bf3b91a9eef06db3da9fa00f365c3075673ad2
Brian Fitch <brianmfitch@gmail.com>
2016-04-16 10:01:07 -0400

    Make commands sync

--- scripts/scaffold.js
+++ scripts/scaffold.js
@@ -1,27 +1,21 @@
 const fs  = require('fs-extra');
 const CWD = process.cwd();
-var exec = require('child_process').exec;
+var exec  = require('child_process').execSync;
 
 module.exports = function scaffold(appName) {
   fs.mkdirs(`${CWD}/${appName}`, function (err) {
     if (err) return console.error(err)
   });
 
-  fs.copy(`${CWD}/scaffold`, `${CWD}/${appName}`, function (err) {
-    if (err) return console.error(err)
-    console.log(`New Cerebral application: ${appName} created!`);
+  var stdout = exec("npm bin",{stdio:[0]});
+  const cliDirectory = `${stdout.toString('utf8').split('/.bin')[0]}/cerebral-cli`;
 
-    process.chdir(appName);
+  fs.copySync(`${cliDirectory}/scaffold`, `${CWD}/${appName}`);
+  console.log(`New Cerebral application: ${appName} created!`);
+  process.chdir(appName);
 
-    exec("git init", function (error, stdout, stderr) {
-      if (error) return console.error(error);
-      console.log(stdout);
-      console.log(stderr);
-    });
-    exec("npm install", function (error, stdout, stderr) {
-      if (error) return console.error(error);
-      console.log(stdout);
-      console.log(stderr);
-    });
-  });
-}
+  stdout = exec('git init', {stdio: [0]});
+  console.log(stdout.toString());
+  stdout = exec('npm install', {stdio: [0]});
+  console.log(stdout.toString());
+};
